00feb68359c978ecb9b48a0363fd085e0ac58182
Eric Chrobak <eric@ericchrobak.com>
2018-06-11 21:37:27 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -17,7 +17,8 @@ class MP3Importer
 
   def files
     list_of_filenames = Dir.entries(@path)
-    list_of_filenames.delete(".").delete("..")
+    list_of_filenames.delete(".")
+    list_of_filenames.delete("..")
     list_of_filenames
   end
 
